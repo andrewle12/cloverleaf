7106be540b384a06ac3f87c3a6713e1eb7d6f05e
Andrew <8879789+manleyac@users.noreply.github.com>
2019-02-05 20:30:52 -0500

    adding server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const routes = require("./routes");
+const db = require("pg");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+if (process.env.NODE_ENV === "production") {
+   app.use(express.static("client/build"));
+ }
+
+ app.use(routes);
+
+ app.listen(PORT, function() {
+    console.log(`App now running on port ${PORT}`);
+ });
