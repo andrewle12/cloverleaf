8e14bd7e0dcb06affac8de2c3a4dba0b2a755459
Des <desinoelle@gmail.com>
2019-02-07 20:56:57 -0500

    Navbar.

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,9 +2,23 @@ import React from "react";
 
 function Nav() {
     return (
-        <div>
-            <h1>CloverLeaf</h1>
-        </div>
+    <div>
+        <h1 className="text-center bg-success text-light">CloverLeaf</h1>
+        <ul className="nav">
+            <li className="nav-item ml-5">
+                <a className="nav-link active" href="/">Home</a>
+            </li>
+            <li className="nav-item ml-5">
+                <a className="nav-link" href="/yard">Yard</a>
+            </li>
+            <li class="nav-item ml-5">
+                <a className="nav-link" href="/posts">Posts</a>
+            </li>
+            <li className="nav-item ml-5">
+                <a className="nav-link disabled" href="/help">Help</a>
+            </li>
+        </ul>
+    </div>
     );
 }
 
