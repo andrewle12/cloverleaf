0c2ef9ab6ec626cb755e668e2c868dc37f43aead
Carl Vega <carljasonvega@gmail.com>
2019-02-14 23:20:42 -0500

    height stretch fix

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,19 +4,38 @@ import FriendsCard from "../components/FriendsCard";
 import Chat from "../components/Chat";
 
 function Home() {
-    return (
-    <Container fluid>
-      <Row>
-        <Col size="md-4">
-          <FriendsCard />
-        </Col>
-        <Col size="md-4">
-          <Chat />
-        </Col>
-        <Col size="md-4">Topics</Col>
-      </Row>
-    </Container>
-    );
+  return (
+    <>
+      <style type="text/css">
+        {`
+    .container-fluid {
+      display: flex;
+      flex-direction: column;
+      height: 100vh;
+    }
+    .row {
+      justify-content: space-around;
+      align-items: stretch;
+      height: 100vh;
+    }
+    .col {
+      height: 100vh;
+    }
+    `}
+      </style>
+      <Container fluid variant="d-flex flex-column h-100">
+        <Row variant="justify-content-around align-items-stretch">
+          <Col size="md-4">
+            <FriendsCard />
+          </Col>
+          <Col size="md-4">
+            <Chat />
+          </Col>
+          <Col size="md-4">Topics</Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
 export default Home;
