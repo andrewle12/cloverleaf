d4237474f11c0da7f88a69c1487b685489730fbd
Charos12 <andrew.le12@gmail.com>
2019-02-16 11:40:01 -0500

    heroku app deploy middleware

--- server.js
+++ server.js
@@ -24,7 +24,8 @@ app.use(express.static("public"));
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("build"));
+  app.get("*", (req, res) => res.sendFile(path.resolve("build", "index.html")));
 }
 
 //setting passport middleware
