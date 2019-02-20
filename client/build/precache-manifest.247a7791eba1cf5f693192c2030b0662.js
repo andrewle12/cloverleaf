caf5df7dc7c48627c0018e56418c77ac2d1c79c4
Carl Vega <carljasonvega@gmail.com>
2019-02-19 22:25:52 -0500

    navbar alignment

---
+++ client/build/precache-manifest.247a7791eba1cf5f693192c2030b0662.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "43709a96b9ff801fdb9a",
+    "url": "/static/js/main.43709a96.chunk.js"
+  },
+  {
+    "revision": "f565fbcfe69e85bfc5a7",
+    "url": "/static/js/2.f565fbcf.chunk.js"
+  },
+  {
+    "revision": "43709a96b9ff801fdb9a",
+    "url": "/static/css/main.7e3eb082.chunk.css"
+  },
+  {
+    "revision": "3b55f013b000a682725fdf0995a99727",
+    "url": "/index.html"
+  }
+];
