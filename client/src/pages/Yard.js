5225d49d69443af81546677be9bb3d0f4cfe20f1
Des <desinoelle@gmail.com>
2019-02-21 19:28:02 -0500

    yard

--- client/src/pages/Yard.js
+++ client/src/pages/Yard.js
@@ -4,7 +4,6 @@ import { connect } from 'react-redux';
 
 import { Container, Row, Col } from "../components/Grid";
 import YardPost from "../components/YardPost";
-import PostMap from "../components/PostMap";
 import { getPosts } from "../redux/actions/posts";
 
 import FriendsCard from "../components/FriendsCard";
@@ -16,7 +15,6 @@ class Yard extends Component{
   }
 
   render() {
-    const { posts } = this.props.post;
 
     return (
       <>
@@ -40,6 +38,10 @@ class Yard extends Component{
     .col-md-3 {
       padding: 0;
       }
+    .yard-post {
+      height: 290px;
+      margin-top: 20px;
+    }
     `}
       </style>
 
@@ -50,10 +52,24 @@ class Yard extends Component{
           </Col>
           <Col size="md-6">
           <YardPost />
-          <div className="card mt-3 mx-1 shadow-sm">
+          <div className="card mt-3 shadow-sm">
             <div className="card-body text-center">
               <h5 className="header card-title">Yard Sales</h5>
-              <PostMap posts={posts}/>
+              <h5><strong>Coming Soon!</strong></h5>
+              <div className="yard-post text-left p-2">
+                <Row>
+                  <Col size="md-6">
+                    <h5>Yard Sale!</h5>
+                    <p>When: Now</p>
+                    <p>Where: My House</p>
+                    <p>What: Really old books</p>
+                  </Col>
+                  <Col size="md-6">
+                    <img src="https://www.roadsideamerica.com/attract/images-icon/wv/WVSHEtinyhouse4_quesada_640x480.jpg" alt="MyHouse" />
+                  </Col>
+                </Row>
+              </div>
+              
             </div>
           </div>
           </Col>
